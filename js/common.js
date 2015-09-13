d61bcf351a8b97948fb1dada8c0a0f043193f344
Chris Vogt <mail@chrisvogt.me>
2015-09-12 19:16:56 -0700

    (front-end) Update masthead styles.

--- js/common.js
+++ js/common.js
@@ -4,24 +4,6 @@
  */
 $(function() {
     /**
-     * Loads the necessary Google Fonts.
-     */
-    WebFontConfig = {
-        google: {
-            families: ['Merriweather:300,300italic:latin', 'Open+Sans:800,700:latin']
-        }
-    };
-    (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-    })();
-
-    /**
      * Init jPages plugin.
      */
     $("div.jHolder").jPages({ /** Initialize jQuery pagination plugin. */
